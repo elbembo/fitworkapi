9fe235ed6fc3ef67abc0c878666922f6e16e9f24
Emad <elbembo@users.noreply.github.com>
2021-06-14 17:44:53 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+const env = process.env;
+
+const config = {
+  db: { /* do not put password or any sensitive info here, done only for demo */
+    host: env.DB_HOST || 'otto.db.elephantsql.com',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER || 'cklijfef',
+    password: env.DB_PASSWORD || 'V1qidES5k3DSJICDRgXtyT8qeu2SPCZp',
+    database: env.DB_NAME || 'cklijfef',
+    ssl: true
+  },
+  listPerPage: env.LIST_PER_PAGE || 20,
+};
+
+module.exports = config;
